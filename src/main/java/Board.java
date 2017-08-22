3024da90ebd18453a54df9031a8eda52eda00838
Colin Biafore <cbia4@users.noreply.github.com>
2017-08-22 14:11:14 -0700

    Updated Board to use the Position object instead of primitive coordinate values

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,7 +17,7 @@ public class Board {
     private final String DOWN = "DOWN";
     private char[][] board;
     private int numShips;
-    private Map<String,Ship> shipPositions;
+    private Map<Position,Ship> shipPositions;
     private List<Ship> ships;
 
     public Board(int size) {
@@ -37,22 +38,17 @@ public class Board {
     // checks validity of ship position and adds a new ship if no conflicts exist
     // ship must be placed within the board bounds with a valid direction given (LEFT, DOWN)
     // ship must not collide with another ship (no overlapping ships/coordinates)
-    public boolean createShip(int length, String rowStart, String colStart, String direction) {
-
-        // translate alphanumeric coordinates
-        int[] coords = GameUtil.strToCoord(rowStart,colStart);
-        int row = coords[0];
-        int col = coords[1];
+    public boolean createShip(int length, Position p, String direction) {
 
         // check for invalid ship parameters
         if (!(direction.equalsIgnoreCase(LEFT) || direction.equalsIgnoreCase(DOWN))) return false;
-        if (row < 0 || col < 0
-                || (row + length > BOARD_SIZE && direction.equalsIgnoreCase(DOWN))
-                || (col + length > BOARD_SIZE && direction.equalsIgnoreCase(LEFT))) return false;
+        if (p.row < 0 || p.col < 0
+                || (p.row + length > BOARD_SIZE && direction.equalsIgnoreCase(DOWN))
+                || (p.col + length > BOARD_SIZE && direction.equalsIgnoreCase(LEFT))) return false;
 
 
         // determine the ships position from input parameters
-        String[] shipPosition = generateShipPositions(row,col,length,direction);
+        Position[] shipPosition = generateShipPositions(p,length,direction);
         if (shipPosition == null) return false;
 
         // create a new ship and add to board
@@ -65,16 +61,18 @@ public class Board {
     // (createShip helper) add new ship positions to the shipPositions map
     private void addShipPositions(Ship ship) {
         this.numShips++;
-        for(String position : ship.getPosition()) {
+        for(Position position : ship.getPosition()) {
             this.shipPositions.put(position,ship);
         }
     }
 
     // (createShip helper) determine the ships position from the input parameters
-    private String[] generateShipPositions(int row, int col, int len, String dir) {
-        String[] shipPosition = new String[len];
+    private Position[] generateShipPositions(Position p, int len, String dir) {
+        Position[] shipPosition = new Position[len];
+        int row = p.row;
+        int col = p.col;
         for(int i = 0; i < len; i++) {
-            shipPosition[i] = GameUtil.coordToStr(row) + Integer.toString(col);
+            shipPosition[i] = new Position(row,col);
             if (this.shipPositions.containsKey(shipPosition[i])) return null; // return null if the coordinate is already taken by another ship
             if (dir.equalsIgnoreCase(LEFT)) col++;
             else row++;
@@ -82,22 +80,24 @@ public class Board {
         return shipPosition;
     }
 
-    // attack the board at the given row (r) and column (c)
-    public String attack(String r, String c) {
 
-        int[] coord = GameUtil.strToCoord(r,c);
-        int row = coord[0];
-        int col = coord[1];
+    // public attack method, row and col are converted to a Position and passed to the private attack method
+    public String attack(String row, String col) {
+        int[] coords = GameUtil.strToCoord(row,col);
+        return attack(new Position(coords[0],coords[1]));
+    }
+
+    // attack the board at the Position specified
+    private String attack(Position p) {
 
         String msg;
-        String position = r + c; // create the symbol representing a board coordinate
 
         // determine the attack result
-        if (row < 0 || col < 0 || row >= BOARD_SIZE || col >= BOARD_SIZE) msg = "Invalid Position";
-        else if(this.board[row][col] != 'X') msg = "Already Taken";
-        else if(shipPositions.containsKey(position)) msg = hitResult(row,col,position);
+        if (p.row < 0 || p.col < 0 || p.row >= BOARD_SIZE || p.col >= BOARD_SIZE) msg = "Invalid Position";
+        else if(this.board[p.row][p.col] != 'X') msg = "Already Taken";
+        else if(shipPositions.containsKey(p)) msg = hitResult(p);
         else {
-            board[row][col] = 'M';
+            board[p.row][p.col] = 'M';
             msg = "Miss";
         }
 
@@ -105,9 +105,9 @@ public class Board {
     }
 
     // decide whether a hit resulted in a simple "Hit", an entire ship "Sunk", or an endgame (i.e. "Win")
-    private String hitResult(int row, int col, String position) {
-        this.board[row][col] = 'H';
-        String msg = this.shipPositions.get(position).hit(position);
+    private String hitResult(Position p) {
+        this.board[p.row][p.col] = 'H';
+        String msg = this.shipPositions.get(p).hit(p);
         if (msg.equals("Hit")) return msg;
         this.numShips--; // if msg != Hit, ship was "Sunk"
         if (this.numShips < 1) msg = "Win"; // if the last ship was sunk, "Win"
